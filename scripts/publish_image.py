ba71522248a13d58bb49d842eda65ec5cbc0646c
RafalStaszak <Ax13jP90!>
2020-05-12 10:45:10 +0000

    publish image

---
+++ scripts/publish_image.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+import cv2
+
+
+def image_publisher():
+    pub = rospy.Publisher('/image', Image, queue_size=10)
+    image_path = rospy.get_param('image_path')
+    scale = rospy.get_param('scale')
+    image = cv2.imread(image_path)
+    width = int(image.shape[1] * scale)
+    height = int(image.shape[0] * scale)
+    image = cv2.resize(image, (width, height))
+    message = CvBridge().cv2_to_imgmsg(image)
+    pub.publish(message)
+
+
+if __name__ == '__main__':
+    rospy.init_node('image_publisher')
+    try:
+        image_publisher()
+    except rospy.ROSInterruptException:
+        pass
